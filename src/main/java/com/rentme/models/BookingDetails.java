9a937f48bf74368822b435ee4ada607f73f194da
Akhil <akhilrao2797@gmail.com>
2020-04-09 21:48:34 +0530

    added models and database setup

--- src/main/java/com/rentme/models/BookingDetails.java
+++ src/main/java/com/rentme/models/BookingDetails.java
@@ -1,5 +1,30 @@
 package com.rentme.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDateTime;
+
+@Entity
 public class BookingDetails {
 
+    @Id
+    String bookingId;
+
+    @ManyToMany
+    Customer customerId;
+
+    @ManyToMany
+    Vehicle vehicle;
+
+    @PastOrPresent
+    LocalDateTime bookingTime;
+
+    @FutureOrPresent
+    LocalDateTime fromTime;
+
+    @FutureOrPresent
+    LocalDateTime toTime;
 }
--- src/main/java/com/rentme/models/Vehicle.java
+++ src/main/java/com/rentme/models/Vehicle.java
@@ -1,4 +1,32 @@
 package com.rentme.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
 public class Vehicle {
+
+    @Id
+    String registrationId;
+
+    @NotEmpty
+    String model;
+
+    @NotEmpty
+    String rc;
+
+    @NotNull
+    long meterReading;
+
+    boolean isFree;
+
+    @NotNull
+    int pricePerLitre;
 }
